de3a6735b1f965ece76eaa11f3697a13b1c7445c
Islom Pardaboyev <154039167+islom-pardaboyev@users.noreply.github.com>
2024-07-30 16:18:30 +0500

    init

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,11 +1,11 @@
- import React from 'react'
+import React from 'react'
  
- function Modal({children, isOpenModal }) {
-   return (
-     <div id='wrapper' className={` ${isOpenModal ? "scale-100" : "scale-0"} duration-300 fixed top-0 bottom-0 right-0 left-0 backdrop-blur-md`}>
-        <div className='w-[500px] p-5 rounded-xl bg-blue-300 mx-auto'>{children}</div>
-     </div>
-   )
- }
- 
- export default Modal
+function Modal({children, isOpenModal, setIsOpenModal }) {
+  return (
+    <div onClick={(e) => e.target.id == 'wrapper' ? setIsOpenModal(false) :""} id='wrapper' className={` ${isOpenModal ? "scale-100" : "scale-0"} duration-300 fixed top-0 bottom-0 right-0 left-0 backdrop-blur-md`}>
+       <div className='w-[500px] p-5 rounded-xl bg-blue-300 mx-auto'>{children}</div>
+    </div>
+  )
+}
+
+export default Modal
